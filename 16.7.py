ab793d5a92eb1e7aa2b889ad77fe5bc95526d1cf
junma <810466205@qq.com>
2022-05-29 17:02:58 +0800

    A

--- 16.7.py
+++ 16.7.py
@@ -5,7 +5,8 @@ screen = pygame.display.set_mode([640,480])
 screen.fill([255, 255, 255])
 for x in range(0, 640):
     y = int(math.sin(x/640 * 4 * math.pi) * 200 + 240)
-    pygame.draw.rect(screen.set_at([x, y], [0, 0, 0]), [0,0,0],[x, y, 1, 1], 1)
+
+    screen.set_at([x, y], [0, 0, 0])
 pygame.display.flip()
 running = True
 while running:
